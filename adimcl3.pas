cbf0700ca5d63ece72cd1b2e6a8658e7d23b8ef6
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2026-01-18 10:26:38 +0100

    Removing dependence

--- adimcl3.pas
+++ adimcl3.pas
@@ -1,7 +1,7 @@
 {
   Description: ADim CL3 vector space utils.
 
-  Copyright (C) 2025 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+  Copyright (C) 2025-2026 Melchiorre Caruso <melchiorrecaruso@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
